7c3dd1a9850a2fe3adfcde87347eb6e052a201f5
HiroMuraki <419734782@qq.com>
2026-04-09 01:37:02 +0800

    为 podman 启动脚本添加了拉取镜像操作，以解决镜像更新

--- podman.sh
+++ podman.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+set -e
+
+if podman pull ghcr.io/hiromuraki/web-letter:latest ; then
+    echo "Successfully pulled the latest image."
+else
+    echo "Failed to pull the latest image. Please check your network connection and try again."
+    exit 1
+fi
 
 podman run --rm -it \
     -p 8000:8000 \
